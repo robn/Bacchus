074499228feb66e24bc74ebd56a40822f5c0fdbc
Robert Norris <rob@eatenbyagrue.org>
2010-08-09 08:17:30 +1000

    stub of a handler

--- lib/Bacchus/Handler/Gadget/GadgetRenderer.pm
+++ lib/Bacchus/Handler/Gadget/GadgetRenderer.pm
@@ -2,8 +2,17 @@ package Bacchus::Handler::Gadget::GadgetRenderer;
 
 use 5.010;
 
-use warnings;
-use strict;
+use namespace::clean;
+
+use Moose;
+use MooseX::Method::Signatures;
+
+method get ( ClassName $class: HashRef $env, HashRef[Str] :$args? = {} ) {
+    use Data::Dumper;
+    return [200, [], [Dumper($args)]];
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
