89add9317833f152eb19388cd8d6069eddd630b6
Robert Norris <rob@eatenbyagrue.org>
2010-08-09 22:42:31 +1000

    start of a gadget class

--- lib/Bacchus/Gadget.pm
+++ lib/Bacchus/Gadget.pm
@@ -6,8 +6,63 @@ use namespace::autoclean;
 
 use Moose;
 use MooseX::Method::Signatures;
+use MooseX::Types::Moose qw(Str);
+
+use Bacchus::Gadget::Feature;
+use Bacchus::Types qw(GadgetFeature);
 
 use XML::Tiny qw(parsefile);
+
+has title              => ( is => "ro", isa => Str, default => '' );
+has author             => ( is => "ro", isa => Str, default => '' );
+has author_email       => ( is => "ro", isa => Str, default => '' );
+has description        => ( is => "ro", isa => Str, default => '' );
+has directory_title    => ( is => "ro", isa => Str, default => '' );
+has screenshot         => ( is => "ro", isa => Str, default => '' );
+has thumbnail          => ( is => "ro", isa => Str, default => '' );
+has title_url          => ( is => "ro", isa => Str, default => '' );
+has author_affiliation => ( is => "ro", isa => Str, default => '' );
+has author_location    => ( is => "ro", isa => Str, default => '' );
+has author_photo       => ( is => "ro", isa => Str, default => '' );
+has author_aboutme     => ( is => "ro", isa => Str, default => '' );
+has author_quote       => ( is => "ro", isa => Str, default => '' );
+has author_link        => ( is => "ro", isa => Str, default => '' );
+has show_stats         => ( is => "ro", isa => Str, default => '' );
+has show_in_directory  => ( is => "ro", isa => Str, default => '' );
+has width              => ( is => "ro", isa => Str, default => '' );
+has height             => ( is => "ro", isa => Str, default => '' );
+has category           => ( is => "ro", isa => Str, default => '' );
+has category2          => ( is => "ro", isa => Str, default => '' );
+has singleton          => ( is => "ro", isa => Str, default => '' );
+has render_inline      => ( is => "ro", isa => Str, default => '' );
+has scaling            => ( is => "ro", isa => Str, default => '' );
+has scrolling          => ( is => "ro", isa => Str, default => '' );
+
+has icon               => ( is => "ro", isa => Str, default => '' );
+
+has _required_features => (
+    traits   => ["Hash"],
+    is       => "ro",
+    isa      => HashRef[GadgetFeature]
+    init_arg => "required_features",
+    default  => sub { {} },
+    handles  => {
+        required_features => 'keys',
+        required_feature  => 'get',
+    },
+);
+
+has _optional_features => (
+    traits   => ["Hash"],
+    is       => "ro",
+    isa      => HashRef[GadgetFeature]
+    init_arg => "optional_features",
+    default  => sub { {} },
+    handles  => {
+        optional_features => 'keys',
+        optional_feature  => 'get',
+    },
+);
 
 method new_from_xml ( ClassName $class: Str $xml ) {
     my %args;
